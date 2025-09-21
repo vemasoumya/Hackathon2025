57181c88f1a6ac3d2186cafe9039518872a44dd8
vemasoumya <101054108+vemasoumya@users.noreply.github.com>
2025-09-21 18:49:59 +0530

    Create table_ddl.sql

---
+++ Leave_SQL/table_ddl.sql
@@ -0,0 +1,9 @@
+create table dbo.LeaveRequests
+(
+EmployeeId  int,
+EmployeeName    varchar(100),
+StartDate       datetime,
+EndDate         datetime,
+NumWorkingDays  smallint,
+Status          varchar(50)
+)
